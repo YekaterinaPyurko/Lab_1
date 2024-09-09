c04d1a2eb32351bcc82dc3cfcfb3e4cc7d7cbea4
YekaterinaPyurko <y_pyurko@kbtu.kz>
2024-09-09 09:04:31 +0500

    Add files via upload

---
+++ lab1/strings.py
@@ -0,0 +1,65 @@
+print("Hello")
+print('Hello')
+
+
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+
+a = "Hello"
+print(a)
+
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+
+#array
+a = "Hello, World!"
+print(a[1])
+
+
+
+#loop
+for x in "banana":
+  print(x)
+
+
+#lenght
+a = "Hello, World!"
+print(len(a))
+
+
+#check
+txt = "The best things in life are free!"
+print("free" in txt)
+
+
+#if
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+
+#notin
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+
+#ifnot
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
